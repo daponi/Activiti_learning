3f8bf3ed03bd9850a50af55b641497b1a6b2da12
liming <ccclane@foxmail.com>
2021-11-10 21:21:12 +0800

    change

--- ActivitiDemo01_bobo/src/test/java/com/bobo/www/test01.java
+++ ActivitiDemo01_bobo/src/test/java/com/bobo/www/test01.java
@@ -1,6 +1,7 @@
 package com.bobo.www;
 
 import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngines;
 import org.junit.Test;
 
@@ -9,10 +10,28 @@ public class test01 {
      * 生成Activiti的相关的表结构
      */
     @Test
-    public void test01(){
+    public void test01() {
+        //定义日志文件的位置为当前项目路径
+        String rootPath = System.getProperty("user.dir");
+        System.setProperty("logPath", rootPath);
+
         // 使用classpath下的activiti.cfg.xml中的配置来创建 ProcessEngine对象
         ProcessEngine defaultProcessEngine = ProcessEngines.getDefaultProcessEngine();
         System.out.println(defaultProcessEngine);
+    }
+
+
+
+    @Test
+    public void test02() {
+        int i = 6;
+
+        if (i > 3) {
+            System.err.println("i>3");
+        } else if (i > 5) {
+
+            System.err.println("i>5");
+        }
     }
 
 }
